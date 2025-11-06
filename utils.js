53253108d8d156152ede6c3df73f92f50be1554c
Bruno <brunovonkoln@gmail.com>
2025-11-06 12:39:08 -0300

    ..

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 function secondsToMinutes(time) {
-  const hours = Math.floor(time / 3600);
+  const hours   = Math.floor(time / 3600);
   const minutes = Math.floor((time % 3600) / 60);
   const seconds = Math.floor(time % 60);
 
